65ecd4f7486d891d107a49c715eff63c3b4603ea
SlashandDash <slashinganddashing@gmail.com>
2024-01-29 17:36:14 -0500

    updated!

--- src/config.hpp
+++ src/config.hpp
@@ -12,8 +12,8 @@ constexpr int paddingY = 3;
 // See https://docs.gtk.org/Pango/type_func.FontDescription.from_string.html
 constexpr const char* font = "Hack 10";
 
-constexpr ColorScheme colorInactive = {Color(0xbb, 0xbb, 0xbb), Color(0, 0, 0)};
-constexpr ColorScheme colorActive = {Color(0xee, 0xee, 0xee), Color(0x20, 0x20, 0x20)};
+constexpr ColorScheme colorInactive = {Color(0xbb, 0xbb, 0xbb), Color(0x22, 0x22, 0x22)};
+constexpr ColorScheme colorActive = {Color(0xee, 0xee, 0xee), Color(0x00, 0x55, 0x77)};
 constexpr const char* termcmd[] = {"foot", nullptr};
 
 static std::vector<std::string> tagNames = {
